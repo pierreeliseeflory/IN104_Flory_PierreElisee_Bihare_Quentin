bdd319bf657e4fa9a0f9bad448f921298d528c07
FLORY Pierre-Elisée <pierre-elisee.flory@ensta-paristech.fr>
2019-03-30 00:23:10 +0100

    add of gitignore and files linted

--- OOP/animal.py
+++ OOP/animal.py
@@ -1,19 +1,19 @@
-class Animals : 
-    def __init__(self,size,weight,hunger_bar,hunger_decrease_rate,alive):
+class Animals:
+    def __init__(self, size, weight, hunger_bar, hunger_decrease_rate, alive):
         self.size = size
         self.weight = weight
         self.hunger_bar = hunger_bar
         self.hunger_decrease_rate = hunger_decrease_rate
         self.alive = True
-    
+
     def tic(self):
         self.hunger_bar -= self.hunger_decrease_rate
-        
 
-class Fish(Animal) :
-    def __init__(self,color,eating_speed):
+
+class Fish(Animal):
+    def __init__(self, color, eating_speed):
         self.color = color
-        self.eating_speed = 0.1 
+        self.eating_speed = 0.1
 
     def get_color(self):
         return self.color
@@ -22,29 +22,29 @@ class Fish(Animal) :
         return self.eating_speed
 
     def eat(self):
-        if self.hunger_bar < 1 :
+        if self.hunger_bar < 1:
             self.hunger_bar += self.eating_speed
 
+
 class Feline(Animal):
-    def __init__(self,speed,on_hunt,sleeping, awake):
+    def __init__(self, speed, on_hunt, sleeping, awake):
         self.speed = speed
         self.awake = True
-        
+
     def get_speed(self):
         return self.speed
 
     def go_on_hunt(self)
-        if self.hunger_bar < 0.3 :
+    if self.hunger_bar < 0.3:
             self.on_hunt = True
 
     def wake_up(self):
         self.awake = True
-    
+
     def eat(self):
-        if self.on_hunt==True and 
-    
+        if self.on_hunt == True and
+
     def sleep(self):
         if hunger_bar > 1:
             self.on_hunt = False
-            self.sleeping = True 
-        
+            self.sleeping = True
